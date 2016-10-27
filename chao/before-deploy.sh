ee8475ce30009ce5e03515297379251a84cfd345
Fred Wang <fred.wang@nugraviton.com>
2016-10-27 11:21:46 -0700

    build

--- chao/before-deploy.sh
+++ chao/before-deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_2b49c16e4094_key -iv $encrypted_2b49c16e4094_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import signingkey.asc
-fi
+#!/bin/bash 
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then 
+    openssl aes-256-cbc -K $encrypted_2b49c16e4094_key -iv $encrypted_2b49c16e4094_iv -in codesigning.asc.enc -out codesigning.asc -d 
+    gpg --fast-import signingkey.asc 
+fi 
+
