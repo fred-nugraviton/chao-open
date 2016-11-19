4880ad26d328744f65994ff94d28aa42001cc7f1
Fred Wang <fred.wang@nugraviton.com>
2016-11-18 16:05:52 -0800

    rename helloworld

--- chao-open-helloworld/src/main/java/com/nugraviton/chao/helloworld/HellloWorld.java
+++ chao-open-helloworld/src/main/java/com/nugraviton/chao/helloworld/HellloWorld.java
@@ -6,18 +6,18 @@ import com.nugraviton.chao.annotation.Job;
 import com.nugraviton.chao.annotation.Task;
 import com.nugraviton.chao.annotation.TriggeredBy;
 
-@Job(name = "hello-world")
+@Job(name="hello-world")
 public class HellloWorld {
 	
 	public static void main(String[] args) throws InterruptedException{
-		Chao core = Chao.create();
-		core.start();
+		Chao chao = Chao.create();
+		chao.start();
 		Thread.sleep(16000);
-		core.shutdown();
-		core.awaitTermination();
+		chao.shutdown();
+		chao.awaitTermination();
 	}
 	
-	@Cron(name ="start-task", jobName = "hello-world", cron="*/5 * * * * *", description="every 5 seconds")
+	@Cron(name="start-task", jobName="hello-world", cron="*/5 * * * * *", description="every 5 seconds")
 	public void helloWorld() throws InterruptedException {
 		//assuming the task takes 5 seconds.
 		Thread.sleep(5000);
@@ -25,7 +25,7 @@ public class HellloWorld {
 	}
 	
 	@TriggeredBy(taskName="start-task")
-	@Task(name ="second-task", jobName = "hello-world", description="a task triggered by [start-task]")
+	@Task(name="second-task", jobName="hello-world", description="a task triggered by [start-task]")
 	public void trailerTask() throws InterruptedException {
 		//assuming the task takes 5 seconds.
 		Thread.sleep(5000);
